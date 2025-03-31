f1c749b62e0577af55c27e471b8ddf28ac772a67
Mayank Gupta <mayankasheshgupta@gmail.com>
2025-03-31 19:11:08 +0530

    removed design pattern

--- frontend/src/components/DesignPattern.jsx
+++
@@ -1,9 +0,0 @@
-// Will come in the login, signup, forgot password and reset password pages
-
-import React from "react";
-
-const DesignPattern = () => {
-  return <div>DesignPattern</div>;
-};
-
-export default DesignPattern;
