5d8adbb7e6250cd88b9053f06c6f4870a7962d71
Mayank Gupta <mayankasheshgupta@gmail.com>
2025-03-31 18:19:06 +0530

    basic folders and files in frontend created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import LoginPage from "./pages/LoginPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+import SignupPage from "./pages/SignupPage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <NavBar />
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+      <Route path="/reset-password" element={<ResetPasswordPage />} />
+    </Routes>
+  );
 };
 
 export default App;
