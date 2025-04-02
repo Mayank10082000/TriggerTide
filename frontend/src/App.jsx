798ec088a20c2334021d3766e0ffba15b2cd028d
Mayank Gupta <mayank.gupta8@stryker.com>
2025-04-02 10:50:11 +0530

    some changes done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -42,7 +42,7 @@ const App = () => {
         />
         <Route
           path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/login" />}
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
         />
         <Route
           path="/create-flow"
